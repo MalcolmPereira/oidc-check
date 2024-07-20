90146aaf7bb5f5385e7b12c14f2ff5e29ee0246c
malcolmpereira <malcolm.j.pereira@gmail.com>
2024-07-20 16:01:29 -0400

    Initial commit of ditribution

---
+++ dist/run.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Make sure the oidc_check-1.0-SNAPSHOT.jar and a configured application.yaml file exists in the directory from where this is run
+
+java -jar oidc_check-1.0-SNAPSHOT.jar --spring.config.name=application --spring.config.location=file:./
